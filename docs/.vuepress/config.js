981de248199b5fb1f378396539cf95b0b10de8fe
hempflower <1993996310@qq.com>
2020-03-21 13:22:21 +0800

    更新配置文件

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: 'SOUI用户手册',
+    description: 'SOUI用户手册',
+    themeConfig: {
+        search: true,
+        lastUpdated: '最后更新于',
+
+        repo: 'hempflower/soui-manual',
+        repoLabel: '查看源码',
+
+        docsRepo: 'hempflower/soui-manual',
+        docsDir: 'docs',
+        docsBranch: 'master',
+        editLinks: true,
+        editLinkText: '改进此页面？',
+
+        sidebar: 'auto'
+    }
+}
